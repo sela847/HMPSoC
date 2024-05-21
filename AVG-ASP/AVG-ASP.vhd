0745ecf788ceda4e9178f899708cfbb2ec6dbce9
BelalHatem <bhat008@aucklanduni.ac.nz>
2024-05-21 16:00:34 +1200

    created a AVG-ASP

---
+++ AVG-ASP/AVG-ASP.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity AVG-ASP is
+    Port (
+        clk      : in  STD_LOGIC;
+        reset    : in  STD_LOGIC;
+        data_in  : in  STD_LOGIC_VECTOR(15 downto 0);
+        data_out : out  STD_LOGIC_VECTOR(15 downto 0);
+        L_sel    : in  STD_LOGIC  -- '0' for L=4, '1' for L=8
+    );
+end AVG-ASP;
+
+architecture behavior of AVG-ASP is
+
+
+end architecture behavior;
