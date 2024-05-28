d6d95a10d1eb1d83f6d0c9e16aaa4d7e217e5fc7
bwei551crazy <crazykwaibao@gmail.com>
2024-05-28 22:19:39 +1200

    Missing receive packet bug is fixed. Now dealing with missing 0 value correlation bug

--- PD-ASP/in_PD.vhd
+++ PD-ASP/in_PD.vhd
@@ -8,6 +8,7 @@ use work.TdmaMinTypes.all;
 entity in_PD is
 	port (
 		clk 	: in std_logic;
+		flag 	: in std_logic;
 		send	: out tdma_min_port;
 		recv 	: in tdma_min_port;
 		correlation : in std_logic_vector(15 downto 0)
@@ -16,15 +17,19 @@ entity in_PD is
 end entity in_PD;
 
 architecture behaviour of in_PD is
-	
+	signal turn_off : std_logic := '0';
 begin
 	send.addr <= x"02";
 	process(clk)
 	begin
-		if rising_edge(clk) then
-			send.data <= "1100" & x"000" & correlation;
-			
+		if rising_edge(clk) then 
+			if flag = '1' then
+				send.data <= "1100" & x"000" & correlation;
+			else
+				send.data <= "1000" & x"000" & correlation;
+			end if;
 		end if;
+		
 	end process;
 end architecture;
 
